f879d8349ad0c32c2de0212336721584d146e639
QueOK <106530636+QueOK@users.noreply.github.com>
2022-06-13 18:12:52 +0700

    Add files via upload

---
+++ Tinh_tong_2_so.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1;
+	int num2;
+	int sum;
+	printf("Nhap các so: \n");
+	scanf("%d",&num1);
+	printf(" + ");
+	scanf("%d", &num2);
+// 	printf(" = ");
+	sum = num1 + num2;
+	printf("sum = %d \n", sum);
+	return 0;
+}
+
+
+
